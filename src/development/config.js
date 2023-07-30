0403e24fa76a25edc5d1c50c3b6ce7ef0acfcad7
panduramadhan2 <panduramadhan2@gmail.com>
2023-07-30 20:21:11 +0700

    test functional trus ngopi

--- src/development/config.js
+++ src/development/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "https://inventorybms-api.onrender.com/"; // Replace with your actual production server URL
-const API_BASE_URL = "https://inventorybms-api.onrender.com/inventories"; // Replace with your actual production server URL
+const API_BASE_URL = "https://inventorybms-api.onrender.com/inventories/"; // Replace with your actual production server URL
 export default API_BASE_URL;

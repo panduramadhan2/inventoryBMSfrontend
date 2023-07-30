b22630b4a9aa13915b25637df8811b377b8f7caa
panduramadhan2 <panduramadhan2@gmail.com>
2023-07-30 18:58:35 +0700

    edit uri backend

--- src/development/config.js
+++ src/development/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "https://inventorybms.onrender.com/"; // Replace with your actual production server URL
+const API_BASE_URL = "https://inventorybms-api.onrender.com/"; // Replace with your actual production server URL
 export default API_BASE_URL;

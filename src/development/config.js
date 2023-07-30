364ab83fb6cb947da32599a4626cea20c5490354
panduramadhan2 <panduramadhan2@gmail.com>
2023-07-30 17:51:44 +0700

    ready to deploy

---
+++ src/development/config.js
@@ -0,0 +1,2 @@
+const API_BASE_URL = "http://127.0.0.1:5000/inventories/"; // Replace with your actual production server URL
+export default API_BASE_URL;
---
+++ src/production/config.js
@@ -0,0 +1,2 @@
+const API_BASE_URL = "https://your-production-api.com"; // Replace with your actual production server URL
+export default API_BASE_URL;

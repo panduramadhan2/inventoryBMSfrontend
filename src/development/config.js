d6cc58bfb608d0d9c3efc00714ee78245758d32a
panduramadhan2 <panduramadhan2@gmail.com>
2023-07-30 18:16:32 +0700

    fix blank white screen cause base url

--- src/development/config.js
+++ src/development/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "http://127.0.0.1:5000/inventories/"; // Replace with your actual production server URL
+const API_BASE_URL = "https://inventorybms.onrender.com/"; // Replace with your actual production server URL
 export default API_BASE_URL;
